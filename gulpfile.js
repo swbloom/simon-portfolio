30ec5c2eb1daa066dcb08914d4412fac636fc454
Simon Bloom <swbloom@gmail.com>
2016-08-20 19:21:36 -0400

    begun work on internal pages

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,26 @@ const concat        = require('gulp-concat');
 const babel         = require('gulp-babel');
 const autoprefixer  = require('gulp-autoprefixer');
 const browserSync   = require('browser-sync').create();
+const injectSvg     = require('gulp-inject-svg');
+const sourcemaps    = require('gulp-sourcemaps');
 const reload        = browserSync.reload;
 
 gulp.task('templates', () => {
-  return gulp.src('./dev/templates/**/*.pug')
-    .pipe(pug())
+  return gulp.src('./dev/templates/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(injectSvg())
     .pipe(gulp.dest('./public/'))
     .pipe(reload({stream: true}));
 })
 
 gulp.task('styles', () => {
   return gulp.src('./dev/styles/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
     .pipe(concat('style.css'))
     .pipe(gulp.dest('./public/styles/'))
     .pipe(reload({stream: true}));
@@ -43,6 +50,7 @@ gulp.task('browser-sync', () => {
 gulp.task('watch', () => {
     gulp.watch('./dev/styles/**/*.scss', ['styles']);
     gulp.watch('./dev/scripts/*.js', ['scripts']);
+    gulp.watch('./dev/templates/*.pug', ['templates']);
 });
 
 gulp.task('default', ['browser-sync', 'styles', 'scripts', 'templates', 'watch']);
