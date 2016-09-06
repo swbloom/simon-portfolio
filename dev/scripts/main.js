ca02138baed912903c89cb24b275be99d466b219
Simon Bloom <swbloom@gmail.com>
2016-09-06 11:48:21 -0400

    Added calendar functionality

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,74 +1,95 @@
 const app = {};
 
-$(document).ready(() => {
+app.sticky = function() {
+  // Sticky Nav
+  const $header = $("header");
+  const $nav = $(".hero-nav-wrapper");
 
-    // Sticky Nav
-    const $header = $("header");
-    const $nav = $(".hero-nav-wrapper");
+  const waypoint =  new Waypoint({
+    element: $('main'),
+    handler: function(direction) {
+      if (direction === "down") {
+        $('body').addClass('stuck');
+        $nav.addClass('sticky');
+        TweenLite.to($nav, 0.4, {
+          top: 0,
+          ease: Power1.easeInOut
+        });
+      } else {
+        TweenLite.to($nav, 0.1, {
+          top: -100,
+          ease: Power1.easeInOut,
+          onComplete: function() {
 
-    // Masonry
-    const $blogRoll = $('.blogroll-articles');
-
-    const waypoint =  new Waypoint({
-      element: $('main'),
-      handler: function(direction) {
-        if (direction === "down") {
-          $('body').addClass('stuck');
-          $nav.addClass('sticky');
-          TweenLite.to($nav, 0.4, {
-            top: 0,
-            ease: Power1.easeInOut
-          });
-        } else {
-          TweenLite.to($nav, 0.1, {
-            top: -100,
-            ease: Power1.easeInOut,
-            onComplete: function() {
-
-              $nav.removeClass('sticky');
-              $('body').removeClass('stuck');
-            }
-          });
-        }
+            $nav.removeClass('sticky');
+            $('body').removeClass('stuck');
+          }
+        });
       }
-    });
-
-    if ($blogRoll.length) {
-      $blogRoll.isotope({
-         itemSelector: '.blogroll-excerpt',
-         layoutMode: 'vertical'
-      });
-
-      $('button.tag').on('click', function(){
-        const filter =  $(this).data('filter');
-        $blogRoll.isotope({filter: filter});
-      });
-
     }
+  });
+}
 
-    $('.calendar').length && $('.calendar').clndr({
-      template:clndrTemplate,
-      daysOfTheWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+app.masonry = function() {
+  // Masonry
+  const $blogRoll = $('.blogroll-articles');
 
+  if ($blogRoll.length) {
+    $blogRoll.isotope({
+       itemSelector: '.blogroll-excerpt',
+       layoutMode: 'vertical'
     });
-});
 
-const clndrTemplate = `
-<div class="clndr-controls">
-    <div class="clndr-previous-button"><img src="images/svgs/calendar-arrow-left.svg"></div>
-    <div class="month"><%= month %> <span class="clndr-year"><%= year %></span></div>
-    <div class="clndr-next-button"><img src="images/svgs/calendar-arrow-right.svg"></div>
-</div>
-<div class="clndr-grid">
-    <div class="days-of-the-week">
-    <% _.each(daysOfTheWeek, function (day) { %>
-        <div class="header-day"><%= day %></div>
-    <% }); %>
-        <div class="days">
-        <% _.each(days, function (day) { %>
-            <div class="<%= day.classes %>"><%= day.day %></div>
-        <% }); %>
-        </div>
-    </div>
-</div>
-`
+    $('button.tag').on('click', function(){
+      const filter =  $(this).data('filter');
+      $blogRoll.isotope({filter: filter});
+    });
+
+  }
+}
+
+app.calendar = function() {
+  app.clndrTemplate = `
+  <div class="clndr-controls">
+      <div class="clndr-previous-button"><img src="images/svgs/calendar-arrow-left.svg"></div>
+      <div class="month"><%= month %> <span class="clndr-year"><%= year %></span></div>
+      <div class="clndr-next-button"><img src="images/svgs/calendar-arrow-right.svg"></div>
+  </div>
+  <div class="clndr-grid">
+      <div class="days-of-the-week">
+      <% _.each(daysOfTheWeek, function (day) { %>
+          <div class="header-day"><%= day %></div>
+      <% }); %>
+          <div class="days">
+          <% _.each(days, function (day) { %>
+              <div class="<%= day.classes %>"><%= day.day %></div>
+          <% }); %>
+          </div>
+      </div>
+  </div>
+  `
+
+  const events = [
+    {
+      startDate: '2016-09-26',
+      endDate: '2016-09-30',
+      title: 'Available'
+    }
+  ]
+
+  $('.calendar').clndr({
+   events: events,
+   multiDayEvents: {
+    endDate: 'endDate',
+    startDate: 'startDate'
+   },
+   template: app.clndrTemplate,
+   daysOfTheWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+ });
+}
+
+$(document).ready(() => {
+    app.sticky();
+    $('.calendar').length && app.calendar();
+    app.masonry();
+});
